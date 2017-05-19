0f6231cdd59d8c822512cfe6875f96807001ce3b
r-furger <r.furger@unibas.ch>
2017-05-19 18:38:53 +0200

    added jobscript

---
+++ 1ex/jobscript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+# 
+# Define Slurm options 
+#SBATCH --job-name=T1 
+#SBATCH --output=T1.out 
+#SBATCH --time=01:00:00 
+#SBATCH --ntasks=1 
+#SBATCH --cpus-per-task=1 
+#SBATCH --mem-per-cpu=4000 
+# GPU settings 
+#SBATCH --partition=titanx 
+#SBATCH --gres=gpu:1
+
+srun T1
